cfd573aed4a45702e38f1f48ec7a93040836e985
LeonZhuk <lionzhuk@gmail.com>
2023-08-19 15:57:06 +0300

    first

---
+++ app/src/main/java/ru/lt/project1_ur/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.lt.project1_ur
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
